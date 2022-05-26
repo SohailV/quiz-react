d2d68198a46690c91f9e3349ed659265a1f948fb
Sohail Virani <virani.sohail02@gmail.com>
2022-05-26 19:48:37 -0400

    intro page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Intro from "./components/Intro"
+import "./App.css";
+import Intro from "./components/Intro";
+import blob1 from "./assets/blob-top.png"
+import blob2 from "./assets/blob-bottom.png"
+
 
 function App() {
   return (
-    
-    <Intro />
-    
-    
+    <section>
+      <img src={blob1} className="img-top" />
+      <img src={blob2} className="img-bottom" />
+      <Intro />
+    </section>
   );
 }
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,18 +1,15 @@
 
-import blob1 from "../assets/blob-top.png"
-import blob2 from "../assets/blob-bottom.png"
 
 
 
 export default function Intro() {
     return(
         <main>
-            <img src={blob1} className="img-top" />
-            <img src={blob2} className="img-bottom"/>
+            
            <div className="intro-content">
-                <h1> Quizzical </h1>
-                <p> Let The Show Begin </p>
-                <button>Start Quiz</button>
+                <h1 className="intro-header"> Quizzical </h1>
+                <p className="tagline"> Let The Show Begin </p>
+                <button className="start-quiz">Start Quiz</button>
             </div>
         </main>
     );
